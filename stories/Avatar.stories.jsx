2308498c64362a8d04643747e8d0ee76a72a4136
Thomas <tx2d07@gmail.com>
2022-11-08 22:10:32 -0500

    nettoyage des dossiers components et fichier index.js

--- stories/Avatar.stories.jsx
+++ stories/Avatar.stories.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 
-import  Avatar  from '../lib/Avatar/Avatar';
+import  Avatar  from '../lib/components/Avatar/Avatar';
 
 export default {
     title: 'Components-TP1/Avatar',
